05aa2d9cc7a0811f0b1eb3a5e1b47808c3c4a0de
Odhi-ambo <michaelderrick605@gmail.com>
2024-11-21 09:33:05 +0300

    main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,55 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:to_do_list/task_manager.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-     
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+
+void main() async{
+  final taskManager = TaskManager();
+  await taskManager.loadTasks();
+
+  while(true){
+    print('---task manager---');
+    print('1. add task');
+    print('2. remove task');
+    print('3. mark as completed');
+    print('4. show tasks');
+    print('5. save tasks');
+    print('6. exit');
+    stdout.write('Enter your choice: ');
+    String? choice = stdin.readLineSync();
+
+    switch(choice){
+      case '1':
+        stdout.write('Enter task name: ');
+        String? name = stdin.readLineSync();
+        stdout.write('Enter task description: ');
+        String? description = stdin.readLineSync();
+        taskManager.addTask(name!, description!);
+        break;
+      case '2':
+        stdout.write('Enter task name: ');
+        String? name = stdin.readLineSync();
+        taskManager.removeTask(name!);
+        break;
+      case '3':
+        stdout.write('Enter task name: ');
+        String? name = stdin.readLineSync();
+        taskManager.markAsCompleted(name!);
+        break;
+      case '4':
+        taskManager.displayTasks();
+        break;
+      case '5':
+         taskManager.saveTasks();
+        break;
+      case '6':
+         taskManager.saveTasks();
+        exit(0);
+        break;
+      default:
+        print('invalid choice');
+    }
   }
 }
 
